415f4f44fd96d9e73174b108b2410e8be9586f59
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-02-26 23:11:20 -0300

    descobri o shift e consegui uma mensagem, mas com alguns caracteres errados

--- Trabalho3/decript/Decript.cpp
+++ Trabalho3/decript/Decript.cpp
@@ -20,11 +20,54 @@ bool loadFreqOrig(string path, vector<char> &alphaList)
     string tmp;
     while (frEncoding >> tmp)
     {
-        cout << tmp << endl;
         int ch = atoi(tmp.c_str());
-        char gg = (size_t) ch;
-        cout << gg << endl;
+        alphaList.push_back(static_cast<char>(ch));
     }
-    
+
     return true;
+}
+stringstream decriptMessage(const stringstream &dataStr, vector<pair<size_t, size_t>> listaFreq, const vector<char> &alphaList)
+{
+    stringstream returnData;
+
+    for (size_t i = 0; i < listaFreq.size(); i++)
+    {
+        listaFreq[i].second = alphaList[i];
+    }
+    int dif = listaFreq.size() - alphaList.size();
+
+    cout << endl
+         << "frqSize: " << listaFreq.size() << " | alphSize: " << alphaList.size() << " | dif: " << dif << endl
+         << endl;
+    int cont = 1;
+    for (size_t i = dif + 1; i < listaFreq.size(); i++)
+    {
+
+        listaFreq[i].second = listaFreq[dif + cont].first;
+        cont++;
+    }
+
+    for (pair<size_t, size_t> f : listaFreq)
+    {
+        cout << (char)f.second << endl;
+    }
+    for (char ch : dataStr.str())
+    {
+        for (size_t i = 0; i < listaFreq.size(); i++)
+        {
+
+            if (ch == (listaFreq[i].first))
+            {
+                returnData << (char)listaFreq[i].second;
+                break;
+            }
+        }
+    }
+    return returnData;
+}
+void printDecription(const stringstream &dataStr, vector<pair<size_t, size_t>> listaFreq, const vector<char> &alphaList)
+{
+    stringstream encriptedMessage;
+    encriptedMessage = decriptMessage(dataStr, listaFreq, alphaList);
+    cout << encriptedMessage.str();
 }
--- Trabalho3/decript/main.cpp
+++ Trabalho3/decript/main.cpp
@@ -36,19 +36,47 @@ int main()
     vector<char> frequenciaOrig;
     if (!loadFreqOrig(pathFreqOrig, frequenciaOrig))
         return 1;
+    /*
+    // descobrir o shift na mão
+    for (int shift = 0; shift < 51; shift++)
+    {
+        cout << endl
+             << "shift = " << shift << endl
+             << endl;
+        vector<char> shiftedAlpha;
+        for (size_t i = 0; i < frequenciaOrig.size(); i++)
+        {
+            char ch = ((size_t)frequenciaOrig[i]) - shift;
+            //cout << ch << " -> " << (char)alfabeto[i] << endl;
+            shiftedAlpha.push_back(ch);
+        }
+
+        printEcription(dataStr, listaFreq, shiftedAlpha);
+        cout << endl;
+    }*/
+    cout << endl
+         << "shift = " << 33 << endl
+         << endl;
+    vector<char> shiftedAlpha;
     for (size_t i = 0; i < frequenciaOrig.size(); i++)
     {
-        cout << frequenciaOrig[i] << " -> " << (char)alfabeto[i] << endl;
+        char ch = ((size_t)frequenciaOrig[i]) - 33;
+        cout << i << " -> " << ch << endl;
+        shiftedAlpha.push_back(ch);
     }
+    printEcription(dataStr, listaFreq, shiftedAlpha);
+    cout << endl;
+    /* 
+    // carrega um alfabeto de um arquivo para decodificar (ou encriptar novamente)
+    if (NDEBUG)
+        cout << "Enter a alphabet" << endl;
+    string pathEncoding;
+    cin >> pathEncoding;
 
-    /*if (listaFreq.size() > alfabeto.size())
-    {
-        if (NDEBUG)
-            cout << "ALfabeto pequeno demais" << endl;
-        return 3;
-    }
-
-    printEcription(dataStr, listaFreq, alfabeto);
+    if (!loadAlphabet(pathEncoding, shiftedAlpha))
+        return 1;
+    printEcription(dataStr, listaFreq, shiftedAlpha);
+    cout << endl;
 */
     return 0;
 }
