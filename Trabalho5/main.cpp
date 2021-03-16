694dee4f5e2da95b7a35774443e3bd55c3664f18
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-03-16 20:20:02 -0300

    adicionei a leitura dos triangulos
    só n faz sentido o meu resultado ser maior que o esperado por 0.01 ....

--- Trabalho5/main.cpp
+++ Trabalho5/main.cpp
@@ -2,35 +2,32 @@
 #include "Generic.hpp"
 #include <vector>
 #include <fstream>
+
+vector<string> splitString(string s, char it)
+{
+    string str;
+    vector<string> ret;
+    for (char c : s)
+    {
+        if (c == it)
+        {
+            ret.push_back(str);
+            str.clear();
+        }
+        else
+        {
+            if (c != ' ')
+                str = str + c;
+        }
+    }
+    ret.push_back(str);
+    return ret;
+}
+
 int main()
 {
     vector<GenericElement> myEls;
-    Circle c1(2);
-    myEls.push_back(c1);
 
-    Square s1(2);
-    myEls.push_back(s1);
-
-    Triangle t1(3, 3, 3);
-    myEls.push_back(t1);
-
-    Point2d p1(0, 0), p2(1, 1), p3(2, 0);
-    Triangle t2(p1, p2, p3);
-    myEls.push_back(t2);
-
-    Sphere sp1(5);
-    myEls.push_back(sp1);
-
-    Point3d p4(-1, -1, -1), p5(5, 0, 3), p6(2.55, 4.1, 1.1), p7(3, 4.45, 1.4142);
-    Tetrahedron tt1(p4, p5, p6, p7);
-    myEls.push_back(tt1);
-
-    Cube cb1(3);
-    myEls.push_back(cb1);
-    for (size_t i = 0; i < myEls.size(); i++)
-    {
-        cout << "Object #" << i << " is " << myEls.at(i).ShowInfo() << endl;
-    }
     if (NDEBUG)
         cout << "NOME DO ARQUIVO PORRA: " << endl;
     string path;
@@ -43,29 +40,48 @@ int main()
         cout << "Erro ao abrir" << endl;
         return 1;
     }
-    while (arq >> aux)
+    while (getline(arq, aux))
     {
         if (aux == "circle")
         {
-            arq >> aux;
+            getline(arq, aux);
             Circle circ(atof(aux.c_str()));
             myEls.push_back(circ);
             continue;
         }
-        if (aux == "triagle")
+        if (aux == "triangle")
         {
+            getline(arq, aux);
+            vector<string> dt = splitString(aux, ',');
+            if (dt.size() == 3)
+            {
+                Triangle tri(atof(dt[0].c_str()), atof(dt[1].c_str()), atof(dt[2].c_str()));
+                myEls.push_back(tri);
+            }
+            else
+            {
+                Point2d pt1(atof(dt[0].c_str()), atof(dt[1].c_str()));
+                getline(arq, aux);
+                dt = splitString(aux, ',');
+                Point2d pt2(atof(dt[0].c_str()), atof(dt[1].c_str()));
+                getline(arq, aux);
+                dt = splitString(aux, ',');
+                Point2d pt3(atof(dt[0].c_str()), atof(dt[1].c_str()));
+                Triangle tri(pt1, pt2, pt3);
+                myEls.push_back(tri);
+            }
             continue;
         }
         if (aux == "sphere")
         {
-            arq >> aux;
+            getline(arq, aux);
             Sphere sph(atof(aux.c_str()));
             myEls.push_back(sph);
             continue;
         }
         if (aux == "square")
         {
-            arq >> aux;
+            getline(arq, aux);
             Square qua(atof(aux.c_str()));
             myEls.push_back(qua);
             continue;
@@ -76,7 +92,7 @@ int main()
         }
         if (aux == "cube")
         {
-            arq >> aux;
+            getline(arq, aux);
             Cube cub(atof(aux.c_str()));
             myEls.push_back(cub);
         }
