b5f84b5ab73faffe2fcbab968fc0517c685e6a8c
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-04-01 20:03:18 -0300

    Pequenas mudanças

--- Trabalho6/Utils.hpp
+++ Trabalho6/Utils.hpp
@@ -6,6 +6,7 @@
 #include "Monstro/Monstro.hpp"
 #include "Pessoa/Pessoa.hpp"
 using namespace std;
+vector<string> splitString(string s, char it);
 bool loadData(vector<string> &dataList, string path);
 void getMonsterData(vector<string> &dataList, vector<Monstro> &monsterList);
 void getItemsData(vector<string> &dataList, Pessoa &hito);
